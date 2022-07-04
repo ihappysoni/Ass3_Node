45455599dae54ad976bc7fcf8d53b762a601fad9
Happy Soni <96763614+ihappysoni@users.noreply.github.com>
2022-07-04 19:37:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express=require("express")
-
 const app=express();
-
 const port=3000;
-
-
 
 const middleware1 = (req, res, next)=>{
     console.log("Global Middleware Used");
     next()
 }
-
 const middleware2 = (req, res, next)=>{
     console.log("Middleware 2 is Used");
     next()
 }
-
 app.use(middleware1);
+
 
 app.get("/",middleware2,(req,res)=>{
     res.send("<h1>Home Page: MiddleWare1 & Middleware2 </h1>")
